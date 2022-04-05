58c7f3ce32040c63afc16de382b12feae455d29f
Reethika <reethikakethireddy@gmail.com>
2022-04-05 15:34:06 -0400

    Initial Commit

---
+++ You are Awesome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You are Awesome
+//
+//  Created by rithu's MAC on 4/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
