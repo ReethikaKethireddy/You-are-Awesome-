d6e234c105e21426d09a45d2eb68eea9bddf0c16
Reethika <reethikakethireddy@gmail.com>
2022-04-19 19:25:01 -0400

    Testing more app options

--- You are Awesome/ViewController.swift
+++ You are Awesome/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var messageLabel1: UILabel!
+    
+    @IBOutlet weak var testImage: UIImageView!
+    
+    //@IBOutlet weak var messageLabel2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("You have entered viewDidLod_fun")
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func questNo1(_ sender: UIButton)
+    {
+        print("Entered button1")
+        messageLabel1.text = "Hello there!!!"
+        //messageLabel2.text = "Diya"
+        testImage.image = UIImage(named: "image2")
+        testImage.contentMode = UIView.ContentMode.scaleAspectFit
+    
+        
+        print("Printed the second image")
+    }
+    
+//    @IBAction func questNo2(_ sender: UIButton)
+//    {
+//        messageLabel1.text = "Aishwarya K"
+//
+//        messageLabel2.text = "chesmusth"
+//        messageLabel2.textColor = UIColor.magenta
+//        //messageLabel2.font = UIFont.
+//    }
+//
+//
+//    @IBAction func clear(_ sender: UIButton)
+//    {
+//        messageLabel1.text = " "
+//        messageLabel2.text = " "
+//    }
 }
+
+//func commenting()
+//{
+//    mbtwecfdg
+//    gqfwcduqgd
+//    hcwdytufqd
+//
+//}
+
 
---
+++ You are Awesome/tableViewContainer.swift
@@ -0,0 +1,19 @@
+//
+//  tableViewContainer.swift
+//  You are Awesome
+//
+//  Created by rithu's MAC on 4/18/22.
+//
+
+import Foundation
+import UIKit
+import UserNotifications
+
+class tableViewContainer{
+    
+    
+
+    
+    
+    
+}
